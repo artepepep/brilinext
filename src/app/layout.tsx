223321c2d48c7a97293a1fd24c57bd63a9fecbbe
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-03-23 13:56:57 +0200

    added some metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import { Footer, Header } from "@/modules";
 
 import "./globals.css";
 import { Provider } from "@/shared/components";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Brilinxoft",
-  description: "Brilinxoft",
+  description: "Brilinxoft is a young company that provides Web and Mobile development, Quality assuranse and testing, UX and UI design. Also with our help you can integrate AI into your apps or ask for a technical support",
 };
 
 export default function RootLayout({
@@ -16,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className="dark:bg-black dark:text-[#D9D9D9] font-sourceSans xl:text-xl xl:leading-[1.4]">
         <Provider>
           <Header />
