6112e7c99546bb75a68cd14bdff1441ca5aeb8bf
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-03-29 11:31:36 +0200

    changed headings

--- src/shared/components/Logotype/Logotype.tsx
+++ src/shared/components/Logotype/Logotype.tsx
@@ -4,14 +4,17 @@ import Link from "next/link";
 export default function Logotype ({ className="text-[28px]" }: { className?: string }) {
   return (
     <Link href={"/"}>
-      <h1 className={`${className} flex items-center font-ysabeau font-semibold text-[32px] dark:text-white`}>
+      <h1 className={`${className} relative flex items-center font-ysabeau font-semibold text-[32px] dark:text-white`}>
         <div className="block dark:hidden">
           <SharedSpriteSVG name="logo-light" />
         </div>
         <div className="hidden dark:block">
           <SharedSpriteSVG name="logo-dark" />
         </div> 
-        rilinxoft
+        <span style={{
+            clip: "rect(1px 1px 1px 1px)"
+          }} 
+          className="absolute h-[1px] w-[1px] overflow-hidden">B</span>rilinxoft
       </h1>
     </Link>
   )
