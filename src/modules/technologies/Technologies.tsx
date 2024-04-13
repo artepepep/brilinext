8cc3c5acf4ae6e488c390577b255229b51e61a02
luckeryouknow <vorozhbytr21@gmail.com>
2024-04-13 13:16:54 +0300

    added effects to marquee

--- src/modules/technologies/Technologies.tsx
+++ src/modules/technologies/Technologies.tsx
@@ -13,6 +13,12 @@ export default function Technologies () {
             <p className="md:max-w-[520px] md:text-xl">Start to build high-grade development solutions for your business purposes using these technologies. From startups and SMEs to large enterprises.</p>
           </div>
           <div className="relative flex flex-col gap-5 xl:ml-auto">
+            <div style={{
+              background: "linear-gradient(89.75deg, black 0.23%, rgba(6, 6, 6, 0) 54%, black 99.8%)",
+            }} className="hidden dark:block absolute h-full w-full z-10"></div>
+            <div style={{
+              background: "linear-gradient(89.75deg, white 0.23%, rgba(250, 250, 250, 0) 54%, white 99.8%);\n",
+            }} className="block dark:hidden absolute h-full w-full z-10"></div>
             <Marquee type="left" />
             <Marquee type="right" />
           </div>
